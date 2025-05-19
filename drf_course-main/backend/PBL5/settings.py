dec0289c3d37b81ece13c7518b9861da43d9873b
TuanVu219 <supersaza219@gmail.com>
2025-05-19 12:22:51 +0700

    Zalopay

--- drf_course-main/backend/PBL5/settings.py
+++ drf_course-main/backend/PBL5/settings.py
@@ -14,10 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 SECRET_KEY = 'django-insecure-abc123xyz!@#'  # Thay bằng key thật của bạn
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = True
 ALLOWED_HOSTS = os.environ.get(
     "DJANGO_ALLOWED_HOSTS",
-    "127.0.0.1 localhost azuredjangotutorial-b6ajcnhzapgwg5e6.southeastasia-01.azurewebsites.net 54.206.226.104"
+    "127.0.0.1 localhost azuredjangotutorial-b6ajcnhzapgwg5e6.southeastasia-01.azurewebsites.net 54.206.226.104",
+    "54.206.226.104"
+    
 ).split(" ")
 ZALOPAY_CONFIG = {
     "app_id": 554,  # Thay bằng app_id của bạn
